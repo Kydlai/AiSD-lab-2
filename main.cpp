ceae78f5a3de79853465f8581d69bb9a5b1a3d6d
Kydlai <kudl2017@mail.ru>
2026-03-19 13:53:59 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "main.h"
+
+using namespace std;
+
+int main(){
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    cout << "Hello, World!";
+
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+#define NOMINMAX
+
+#include <iostream>
+#include <windows.h>
+
+#endif
